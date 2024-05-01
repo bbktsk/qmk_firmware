4d8aa52430f603d8334829db68d95eae41dd18c4
Bob Koutsky <bk@bbktsk.io>
2024-05-01 10:52:16 +0200

    wip

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@ set -e
 
 KMAP=./keyboards/splitkb/aurora/sofle_v2/keymaps/bbktsk
 
-qmk json2c $KMAP/keymap.json
+qmk json2c $KMAP/keymap-layout.json > $KMAP/keymap.c
+cat $KMAP/keymap-code.c >> $KMAP/keymap.c
+
+qmk compile
